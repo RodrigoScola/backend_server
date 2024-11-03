17f88fb6c507e6dfb6d50391104aef2d6079b629
Rodrigo Scola <rodrigo.sgarabotto.scola@gmail.com>
2024-11-03 16:18:54 -0300

    add port fallback

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 
 export const __DEV__ = process.env.NODE_ENV === 'development';
 export const __PROD__ = process.env.NODE_ENV === 'production';
